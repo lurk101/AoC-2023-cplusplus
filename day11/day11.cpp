5ed14325a44fd6c2c13d372192a64cc06d9b5a7b
lurk101 <lurkone0one@gmail.com>
2024-01-05 08:39:40 -0500

    optimize day 11

--- day11/day11.cpp
+++ day11/day11.cpp
@@ -3,7 +3,6 @@ constexpr auto title = "--- Day 11: Cosmic Expansion ---";
 #include <chrono>
 #include <fstream>
 #include <iostream>
-#include <set>
 #include <string>
 #include <vector>
 
@@ -11,48 +10,63 @@ using namespace std;
 using namespace chrono;
 
 static vector<string> lines;
-static vector<bool> empty_columns;
 
-static bool empty_row(string row) {
-    for (char c : row)
-        if (c != '.') return false;
-    return true;
+static int count_row(int row) {
+    int count = 0;
+    for (char c : lines[row])
+        if (c == '#')
+            count++;
+    return count;
 }
 
-static bool empty_col(vector<string>& lines, int col) {
+static int count_col(int col) {
+    int count = 0;
     for (string& row : lines)
-        if (row[col] != '.') return false;
-    return true;
-}
-
-static int manhattan(pair<int, int>& a, pair<int, int>& b) {
-    return abs(a.first - b.first) + abs(a.second - b.second);
+        if (row[col] == '#')
+            count++;
+    return count;
 }
 
 int64_t part(uint32_t delta) {
-    vector<pair<int, int>> galaxy;
-    uint32_t y_inc = 0;
+    vector<int> row_pos, row_count, col_pos, col_count;
+    int y_inc = 0;
     for (int y = 0; y < lines.size(); y++) {
-        if (empty_row(lines[y])) y_inc += delta;
-        int x_inc = 0;
-        for (int x = 0; x < lines[0].size(); x++) {
-            if (empty_columns[x]) x_inc += delta;
-            if (lines[y][x] == '#') galaxy.push_back(make_pair(y + y_inc, x + x_inc));
+        int row_n = count_row(y);
+        if (row_n == 0)
+            y_inc += delta;
+        else {
+            row_pos.push_back(y + y_inc);
+            row_count.push_back(row_n);
         }
     }
-    int64_t sum = 0;
-    for (int i = 0; i < galaxy.size() - 1; i++)
-        for (int j = i + 1; j < galaxy.size(); j++) sum += manhattan(galaxy[i], galaxy[j]);
-    return sum;
+    int x_inc = 0;
+    for (int x = 0; x < lines[0].size(); x++) {
+        int col_n = count_col(x);
+        if (col_n == 0)
+            x_inc += delta;
+        else {
+            col_pos.push_back(x + x_inc);
+            col_count.push_back(col_n);
+        }
+    }
+    int64_t dist = 0;
+    for (int y = 0; y < row_pos.size() - 1; y++)
+        for (int y2 = y + 1; y2 < row_pos.size(); y2++)
+            dist +=
+                abs(row_pos[y] - row_pos[y2]) * row_count[y] * row_count[y2];
+    for (int x = 0; x < col_pos.size() - 1; x++)
+        for (int x2 = x + 1; x2 < col_pos.size(); x2++)
+            dist +=
+                abs(col_pos[x] - col_pos[x2]) * col_count[x] * col_count[x2];
+    return dist;
 }
 
 int main() {
     auto start = high_resolution_clock::now();
     ifstream f("day11.txt");
     string line;
-    while (getline(f, line)) lines.push_back(line);
-    for (int x = 0; x < lines[0].size(); x++)
-        empty_columns.push_back(empty_col(lines, x));
+    while (getline(f, line))
+        lines.push_back(line);
     cout << title << endl
          << "Part 1  - " << part(1) << endl
          << "Part 2  - " << part(999999) << endl
